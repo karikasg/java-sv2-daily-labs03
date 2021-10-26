a6bfb92ebe0f3633f8c8be8680e54468e97950bc
karikasg <karikas.gabor@gmail.com>
2021-10-26 15:48:07 +0200

    Week03 day02 labs

--- src/main/java/day02/Mathematics.java
+++ src/main/java/day02/Mathematics.java
@@ -2,9 +2,9 @@ package day02;
 
 public class Mathematics {
     public boolean isPrime(int number){
-        if ((number % 2) == 0) {return false;};
-        for (int i=3; i<Math.sqrt((double) number); i=i+2){
-            if ((number % i) == 0) {return false;};
+        if ((number % 2) == 0) {return false;}
+        for (int i=3; i<Math.sqrt(number); i=i+2){
+            if ((number % i) == 0) {return false;}
         }
         return true;
     }
