27c8619598b27218172a6e2ea65b56413e52b18d
karikasg <karikas.gabor@gmail.com>
2021-10-28 11:26:52 +0200

    Week03 day04 Szultán börtön

---
+++ src/main/java/day04/Szultan.java
@@ -0,0 +1,17 @@
+package day04;
+
+public class Szultan {
+    public static void main(String[] args) {
+        boolean[] prison = new boolean[100];
+
+        for (int i = 1; i <= 100; i++){
+            for (int j = i; j<= 100; j += i){
+                prison[j-1] = !prison[j-1];  //utálom azokat a nyelveket, ahol 0-tól kezdődik az index
+            }
+        }
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i + (prison[i-1] ? ": nyitva" : ": zárva"));
+        }
+    }
+}
+// a kiírt eredmény alapján megállapítható, hogy a négyzetszám számú cellákból szabadulnak a rabok.
